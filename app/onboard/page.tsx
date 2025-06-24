0b5fef464cf68b2cd468ac9b4caa6418ddfd6a6e
Anshu kumar Mandal <anshukrmandal7007@gmail.com>
2025-06-25 00:00:34 +0530

    edited

--- app/onboard/page.tsx
+++ app/onboard/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useForm, Controller } from "react-hook-form";
+import { useForm} from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
